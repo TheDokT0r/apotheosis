e017892db495fc2b927323bee5db3618dbc5bfc3
Orche <orkv1234@gmail.com>
2024-05-10 00:34:42 +0300

    created basic login form

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./main.scss";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+const Login = lazy(() => import("@/routes/Login/Login"));
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Suspense fallback={<p>Loading...</p>} />
+        <Routes>
+          <Route path="/" element={<h1>Hello, World!</h1>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/routes/Login/Login.tsx
@@ -0,0 +1,48 @@
+import { Button, FormControl, TextField } from "@mui/material";
+import "./Login.scss";
+import { useState } from "react";
+import LoginIcon from "@mui/icons-material/Login";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onLoginPress = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(email, password);
+  };
+
+  return (
+    <FormControl variant="outlined" className="login-form">
+      <h1>Login to start your adventure!</h1>
+      <TextField
+        sx={{ margin: "2rem" }}
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        label="Email"
+      />
+      <TextField
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+      />
+
+      <Button
+        onClick={onLoginPress}
+        variant="contained"
+        sx={{ marginTop: "2rem" }}
+      >
+        <LoginIcon />
+        Login
+      </Button>
+
+      <p>
+        Don't have an account yet?{" "}
+        <a onClick={() => navigate("/signup")}>Create one now!</a>
+      </p>
+    </FormControl>
+  );
+}
